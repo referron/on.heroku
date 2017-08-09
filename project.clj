d0f8f9374fd193ca9739c8e53bf4730b4a658855
Chris Zheng <z@caudate.me>
2017-08-09 10:55:00 +1000

    added datomic buildpack

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-getting-started "1.0.0-SNAPSHOT"
+(defproject referron/on.heroku "1.0.0-SNAPSHOT"
   :description "Demo Clojure web app"
   :url "https://github.com/referron/on.heroku"
   :license {:name "Eclipse Public License v1.0"
@@ -6,9 +6,14 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [environ "1.0.0"]]
+                 [environ "1.0.0"]
+                 [com.datomic/datomic-pro "0.9.5561.54"
+                  :exclusions [[com.google.guava/guava]]]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "on.heroku.jar"
-  :profiles {:production {:env {:production true}}})
+  :profiles {:production {:env {:production true}}}
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :username "accounts@referron.com"
+                                   :password "649a2f50-b4cd-412f-a75d-9a6c45d49418"}})
